701fb32aab2ba60274af7b6609a12374a4869839
uchihamadararara <royal2781925@gmail.com>
2026-08-27 20:36:12 +0530

    Delete app directory

--- app/applet/app/src/main/java/com/example/domain/repository/AuthRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.repository
-
-import kotlinx.coroutines.flow.Flow
-
-interface AuthRepository {
-    val currentUserId: Flow<String?>
-    suspend fun signInAnonymously(): Result<String> // Used as a fallback if Google sign in isn't requested yet
-    suspend fun signOut()
-}
